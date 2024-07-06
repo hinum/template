4cd0fc8a969ac9f313935defb27b9b960f17ab04
hinum <sb5719@sb-school.ac.th>
2024-07-06 13:14:50 +0700

    some more helper funcs

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,16 @@
 import { Comp, EventController, Vec2 } from "kaboom";
 import { k } from "./main";
 
+type allowedKey = string | number | symbol
+export const entries = <K extends allowedKey,T>(obj: Record<K,T>)=>Object.entries(obj) as [K,T][]
+export const fromEntries = <K extends allowedKey,T>(arr: [K,T][])=>Object.entries(arr) as Record<K,T>
+export const keys = <K extends allowedKey>(obj: Record<K, unknown>)=>Object.keys(obj) as K[]
+export const values = <T>(obj: Record<any, T>)=>Object.values(obj) as T[]
+
+export function print<T>(inp: T){
+  console.log(inp)
+  return inp
+}
 export const promisify = <T>(fn: (handler: (e: T)=>void)=>EventController)=>new Promise<T>(res=>{
   const handle = fn((arg)=>{
     handle.cancel()
